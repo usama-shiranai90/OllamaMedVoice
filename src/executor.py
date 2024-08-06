11a9be2fb4eaf68e5f2419105181fa48c122bc78
JobayerChowdhury <arnobchowdhury82@gmail.com>
2024-08-06 01:04:24 -0900

    Resolved merge conflicts

--- src/executor.py
+++ src/executor.py
@@ -12,7 +12,7 @@ language = 'en'
 # from os.path import join, dirname
 # import matplotlib.pyplot as plt
 # ^ matplotlib is great for visualising data and for testing purposes but usually not needed for production
-openai.api_key='sk-proj-Vq0Vy934245Z61sIJ2Pl2aT3BlbkFJxuPQuVnOydiXgQ8Uhbqz'
+openai.api_key=''
 # load_dotenv()
 model = 'gpt-3.5-turbo'
 r = sr.Recognizer()
