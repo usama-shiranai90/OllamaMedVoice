1ff2b7e1436701f262dc170a0f1711bb1239014a
OneEyeOwl-19 <53114169+usama-shiranai90@users.noreply.github.com>
2024-08-23 20:15:27 +0900

    + Improve TTS
    - Excluding Japanese content.
    - Text to Speech (Japanese)

--- src/owl-module.py
+++ src/owl-module.py
@@ -1,4 +1,5 @@
 import os
+import time
 from enum import Enum
 from random import choice
 from dotenv import load_dotenv
@@ -9,9 +10,13 @@ from gtts import gTTS
 import speech_recognition as sr
 from vosk import Model, KaldiRecognizer
 
-from langchain.prompts import PromptTemplate
-from langchain_openai import OpenAI
-from langchain.chains import LLMChain
+# from langchain.prompts import PromptTemplate
+# from langchain_openai import OpenAI
+# from langchain.chains import LLMChain
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI  # OpenAI
+from langchain_core.runnables import RunnableSequence
+from langchain_core.output_parsers import StrOutputParser
 
 from typing import Optional
 
@@ -89,38 +94,17 @@ def text_generation(text):
             template=prompt_templates.get(t, prompt_templates["social_tech_lab"])
         )
 
-    llm = OpenAI(model='gpt-3.5-turbo-instruct', temperature=0)
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
     topic = determine_topic(text)
     if not topic:
         return "Sorry, I couldn't determine the appropriate topic for your query."
 
-    prompt_template = get_prompt_template(topic)
-    llm_chain = LLMChain(llm=llm, prompt=prompt_template)
-    response_text = llm_chain.invoke({'question': text})['text']
+    summarizing_prompt = get_prompt_template(topic)
+    summarizing_chain = summarizing_prompt | llm | StrOutputParser()
+    print(topic, prompt_templates)
+    response_text = summarizing_chain.invoke({'question': text})
     print(response_text)
     return response_text
-
-    # social_tech_lab = """
-    # You are an ice cream assistant chatbot named "Scoopsie". Your expertise is
-    # exclusively in providing information and advice about anything related to ice creams. This includes flavor combinations, ice cream recipes, and general
-    # ice cream-related queries. You do not provide information outside of this
-    # scope. If a question is not about ice cream, respond with, "I specialize only in ice cream related queries."
-    # Question: {question}
-    # Answer:"""
-    #
-    # ice_cream_assistant_prompt_template = PromptTemplate(
-    #     input_variables=["question"],
-    #     template=social_tech_lab
-    # )
-    # llm = OpenAI(model='gpt-3.5-turbo-instruct', temperature=0)
-    #
-    # llm_chain = LLMChain(llm=llm, prompt=ice_cream_assistant_prompt_template)
-    #
-    # # response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": f"{text}"}])
-    # # response_text = response.choices[0].message.content
-    # response_text = llm_chain.invoke({'question': text})['text']
-    # print(response_text)
-    # return response_text
 
 
 def self_listen():
@@ -137,13 +121,25 @@ def self_listen():
 
 def text_to_speak(text, language=Language.ENGLISH.value):
     if language is Language.ENGLISH.value:
-        response = gTTS(text=text, lang='en')
+        response = gTTS(text=text, lang='en', tld='com.au')
     else:
         response = gTTS(text=text, lang='ja')
-    file = "raw/raw.mp3"  # TODO: Change to .wav on LINUX
-    response.save(file)
-    playsound.playsound(file)
-    # os.system(f"start {file}")
+
+    directory = "raw"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    file = os.path.join(directory, "raw.mp3")  # TODO: Change to .wav on LINUX
+
+    try:
+        response.save(file)
+        playsound.playsound(file)
+        # os.system(f"start {file}")
+    except PermissionError as e:
+        # print("KILLING FILE")
+        os.remove(file)
+        response.save(file)
+        playsound.playsound(file)
+    time.sleep(0.5)
 
 
 def speech_to_text(audio, language=Language.ENGLISH.value):
@@ -212,7 +208,7 @@ def dynamic_check(audio, up_and_running):
             undetectable = True
             is_started = False
 
-        print("==>", text.lower() in change_language_words, undetectable, is_started, up_and_running)
+        # print("==>", text.lower() in change_language_words, undetectable, is_started, up_and_running)
         if is_started and not up_and_running:
             wake_words_for_language = wake_words.get("English") if language == english else wake_words.get(
                 "Japanese")
@@ -226,7 +222,7 @@ def dynamic_check(audio, up_and_running):
                 else:
                     text_to_speak(choice(greetings_dict.get("Japanese")), japanese)
         elif is_started and up_and_running:
-            print("Up and Running Normal conversation in {} = {}".format(language, default_language))
+            # print("Up and Running Normal conversation in {} = {}".format(language, default_language))
             if default_language == Language.ENGLISH.value:
                 response = text_generation(text)
                 text_to_speak(response)
@@ -259,17 +255,17 @@ def wake_up_call():
     global default_language
     with m as source:
         r.adjust_for_ambient_noise(source)
-    print("Set minimum energy threshold to {}".format(r.energy_threshold))
+    # print("Set minimum energy threshold to {}".format(r.energy_threshold))
     up_and_running = False
     while True:
         audio = self_listen()
         if audio:
             message, undetectable, up_and_running = dynamic_check(audio, up_and_running)
             if undetectable:
-                text_to_speak(choice(greetings_dict.get(
-                    "English")))  # f"{message} Please select which language you want to communicate?"
+                text_to_speak(choice(greetings_dict.get("English")))
                 break
 
+    print("here we are", undetectable, up_and_running)
     normal_state = False
     if undetectable:  # Manual language checker.
         while True:
@@ -301,137 +297,137 @@ def wake_up_call():
 
 if __name__ == "__main__":
     env = load_dotenv()
-    response = text_generation("What do you know about social tech lab?")
-    text_to_speak(response)
+    name = "Social Tech Lab"
+    wake_words = {
+        "English": [
+            "hey", "hello", "hellos", "hellow"
+                                      "hey bot", "hello assistant",
+            "wake up",
+            "waked up",
+            "start up",
+            "activate",
+            "wake up please",
+            "are you there",
+            "wake up now",
+            "rise and shine",
+            "is anyone there?",
+            "wake up bot",
+        ],
+        "Japanese": [
+            "起きて",  # Okite - Wake up
+            "起動して",  # Kidō shite - Start up
+            "目を覚まして",  # Me o samashite - Open your eyes / Wake up
+            "反応して",  # Hannō shite - Respond
+            "活動を開始して",  # Katsudō o kaishi shite - Start activity
+            "動作開始",  # Dōsa kaishi - Begin operation
+            "応答して",  # Ōtō shite - Reply
+            "目覚めて",  # Mezamete - Wake up
+            "スリープ解除",  # Surīpu kaijo - Exit sleep mode
+            "起き上がって",  # Okiagatte - Get up
+            "始動して",  # Shidō shite - Power on
+            "アクティブにして",  # Akutibu ni shite - Become active
+            "作動して",  # Sadō shite - Operate
+            "起動",  # Kidō - Startup
+            "スリープモードを解除",  # Surīpu mōdo o kaijo - Cancel sleep mode
+            "起きろ",  # Okiro - Wake up (casual)
+            "目を開けて",  # Me o akete - Open your eyes
+            "起き上がれ",  # Okiagare - Get up (casual)
+            "スタートして",  # Sutāto shite - Start
+            "起きよう",  # Okirō - Let’s wake up
+            "動き出して",  # Ugokidashite - Start moving
+            "こんにちはロボット", "聞いて", "ハローアシスタント", "おーい", "注目",
+            "こんにちは", "おはよう", "おはようございます", "もしもし", "やあ", "こんばんは"]
+    }
+    change_language = {
+        "English": [
+            "manual", "manual change", "manual changed",
+            "change language",
+            "switch language",
+            "alter language",
+            "change the language",
+            "switch the language",
+            "set language",
+            "change language settings",
+            "update language",
+            "select language",
+            "modify language",
+            "language options",
+            "change to english",
+            "change to japanese",
+            "switch to english",
+            "switch to japanese",
+            "change default language",
+            "set default language",
+            "manually change language",
+            "adjust language",
+            "change interface language",
+            "choose language",
+            "change system language",
+            "language change",
+            "language selection",
+            "change to another language",
+            "switch the default language",
+            "update language settings",
+            "set the language",
+            "configure language",
+            "select a new language",
+            "switch language mode",
+            "use english",
+            "use japanese",
+            "language shift",
+            "change the spoken language",
+        ],
+        "Japanese": [
+            "言語を変更",  # Gengo o henkō - Change language
+            "言語を切り替え",  # Gengo o kirikae - Switch language
+            "言語を変えて",  # Gengo o kaete - Change the language
+            "言語の変更",  # Gengo no henkō - Language change
+            "言語設定を変更",  # Gengo settei o henkō - Change language settings
+            "言語の切り替え",  # Gengo no kirikae - Language switch
+            "言語を手動で変更",  # Gengo o shudō de henkō - Manually change language
+            "新しい言語を設定",  # Atarashī gengo o settei - Set a new language
+            "使用言語を変更",  # Shiyō gengo o henkō - Change the language used
+            "デフォルト言語を変更",  # Deforuto gengo o henkō - Change the default language
+            "インターフェース言語を切り替え",  # Intāfēsu gengo o kirikae - Switch interface language
+            "言語を選択",  # Gengo o sentaku - Select language
+            "日本語に変更",  # Nihongo ni henkō - Change to Japanese
+            "英語に変更",  # Eigo ni henkō - Change to English
+            "言語オプションを変更",  # Gengo opushon o henkō - Change language options
+            "言語設定を変更してください",  # Gengo settei o henkō shite kudasai - Please change the language settings
+            "言語を英語にして",  # Gengo o eigo ni shite - Change the language to English
+            "言語を日本語にして",  # Gengo o nihongo ni shite - Change the language to Japanese
+            "使用中の言語を変更",  # Shiyō-chū no gengo o henkō - Change the currently used language
+            "マニュアルで言語を変えて",  # Manyuaru de gengo o kaete - Change the language manually
+        ]
+    }
+    greetings_dict = {
+        "English": [
+            f"Welcome to {name}, You have selected English.",
+            f"Hey There, Are you excited about {name}? To continue, you have selected English.",
+            f"Well, hello there, how's it going today in the {name}? You have selected English."
+        ],
+        "Japanese": [
+            f"{name}へようこそ、あなたは日本語を選択しました。",
+            f"こんにちは、{name}に興奮していますか？ 続けるには、日本語を選択しました。",
+            f"こんにちは、{name}での今日はどうですか？ あなたは日本語を選択しました。"
+        ]
+    }
 
-    # default_language = Language.ENGLISH.value
-    # is_started = False
-    # wake_words = {
-    #     "English": [
-    #         "hey", "hello", "hellos", "hellow"
-    #                                   "hey bot", "hello assistant",
-    #         "wake up",
-    #         "waked up",
-    #         "start up",
-    #         "activate",
-    #         "wake up please",
-    #         "are you there",
-    #         "wake up now",
-    #         "rise and shine",
-    #         "is anyone there?",
-    #         "wake up bot",
-    #     ],
-    #     "Japanese": [
-    #         "起きて",  # Okite - Wake up
-    #         "起動して",  # Kidō shite - Start up
-    #         "目を覚まして",  # Me o samashite - Open your eyes / Wake up
-    #         "反応して",  # Hannō shite - Respond
-    #         "活動を開始して",  # Katsudō o kaishi shite - Start activity
-    #         "動作開始",  # Dōsa kaishi - Begin operation
-    #         "応答して",  # Ōtō shite - Reply
-    #         "目覚めて",  # Mezamete - Wake up
-    #         "スリープ解除",  # Surīpu kaijo - Exit sleep mode
-    #         "起き上がって",  # Okiagatte - Get up
-    #         "始動して",  # Shidō shite - Power on
-    #         "アクティブにして",  # Akutibu ni shite - Become active
-    #         "作動して",  # Sadō shite - Operate
-    #         "起動",  # Kidō - Startup
-    #         "スリープモードを解除",  # Surīpu mōdo o kaijo - Cancel sleep mode
-    #         "起きろ",  # Okiro - Wake up (casual)
-    #         "目を開けて",  # Me o akete - Open your eyes
-    #         "起き上がれ",  # Okiagare - Get up (casual)
-    #         "スタートして",  # Sutāto shite - Start
-    #         "起きよう",  # Okirō - Let’s wake up
-    #         "動き出して",  # Ugokidashite - Start moving
-    #         "こんにちはロボット", "聞いて", "ハローアシスタント", "おーい", "注目",
-    #         "こんにちは", "おはよう", "おはようございます", "もしもし", "やあ", "こんばんは"]
-    # }
-    # change_language = {
-    #     "English": [
-    #         "manual", "manual change", "manual changed",
-    #         "change language",
-    #         "switch language",
-    #         "alter language",
-    #         "change the language",
-    #         "switch the language",
-    #         "set language",
-    #         "change language settings",
-    #         "update language",
-    #         "select language",
-    #         "modify language",
-    #         "language options",
-    #         "change to english",
-    #         "change to japanese",
-    #         "switch to english",
-    #         "switch to japanese",
-    #         "change default language",
-    #         "set default language",
-    #         "manually change language",
-    #         "adjust language",
-    #         "change interface language",
-    #         "choose language",
-    #         "change system language",
-    #         "language change",
-    #         "language selection",
-    #         "change to another language",
-    #         "switch the default language",
-    #         "update language settings",
-    #         "set the language",
-    #         "configure language",
-    #         "select a new language",
-    #         "switch language mode",
-    #         "use english",
-    #         "use japanese",
-    #         "language shift",
-    #         "change the spoken language",
-    #     ],
-    #     "Japanese": [
-    #         "言語を変更",  # Gengo o henkō - Change language
-    #         "言語を切り替え",  # Gengo o kirikae - Switch language
-    #         "言語を変えて",  # Gengo o kaete - Change the language
-    #         "言語の変更",  # Gengo no henkō - Language change
-    #         "言語設定を変更",  # Gengo settei o henkō - Change language settings
-    #         "言語の切り替え",  # Gengo no kirikae - Language switch
-    #         "言語を手動で変更",  # Gengo o shudō de henkō - Manually change language
-    #         "新しい言語を設定",  # Atarashī gengo o settei - Set a new language
-    #         "使用言語を変更",  # Shiyō gengo o henkō - Change the language used
-    #         "デフォルト言語を変更",  # Deforuto gengo o henkō - Change the default language
-    #         "インターフェース言語を切り替え",  # Intāfēsu gengo o kirikae - Switch interface language
-    #         "言語を選択",  # Gengo o sentaku - Select language
-    #         "日本語に変更",  # Nihongo ni henkō - Change to Japanese
-    #         "英語に変更",  # Eigo ni henkō - Change to English
-    #         "言語オプションを変更",  # Gengo opushon o henkō - Change language options
-    #         "言語設定を変更してください",  # Gengo settei o henkō shite kudasai - Please change the language settings
-    #         "言語を英語にして",  # Gengo o eigo ni shite - Change the language to English
-    #         "言語を日本語にして",  # Gengo o nihongo ni shite - Change the language to Japanese
-    #         "使用中の言語を変更",  # Shiyō-chū no gengo o henkō - Change the currently used language
-    #         "マニュアルで言語を変えて",  # Manyuaru de gengo o kaete - Change the language manually
-    #     ]
-    # }
-    #
-    # # model_path = "vosk-model-ja-0.22"
-    # # model = Model(model_path)
-    # # recognizer = KaldiRecognizer(model, 16000)
-    # # Set up the speech recognition and text-to-speech engines
-    # # r = sr.Recognizer()
-    #
-    # name = "Social Tech Lab"
-    # greetings_dict = {
-    #     "English": [
-    #         f"Welcome to {name}, You have selected English.",
-    #         f"Hey There, Are you excited about {name}? To continue, you have selected English.",
-    #         f"Well, hello there, how's it going today in the {name}? You have selected English."
-    #     ],
-    #     "Japanese": [
-    #         f"{name}へようこそ、あなたは日本語を選択しました。",
-    #         f"こんにちは、{name}に興奮していますか？ 続けるには、日本語を選択しました。",
-    #         f"こんにちは、{name}での今日はどうですか？ あなたは日本語を選択しました。"
-    #     ]
-    # }
-    #
+    # response = "What do you know about social tech lab Kyushu university?"
+    # # response = text_generation("What do you know about social tech lab Kyushu university?")
+    # text_to_speak(response)
+
+    default_language = Language.ENGLISH.value
+    is_started = False
+    # model_path = "vosk-model-ja-0.22"
+    # model = Model(model_path)
+    # recognizer = KaldiRecognizer(model, 16000)
+    # Set up the speech recognition and text-to-speech engines
     # r = sr.Recognizer()
-    # m = sr.Microphone(device_index=2)
-    # try:
-    #     wake_up_call()
-    # except KeyboardInterrupt:
-    #     pass
+
+    r = sr.Recognizer()
+    m = sr.Microphone(device_index=2)
+    try:
+        wake_up_call()
+    except KeyboardInterrupt:
+        pass
